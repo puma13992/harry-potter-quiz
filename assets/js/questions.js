0cee3b15c833409e13dffb13473a5b301671f6de
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-06-03 08:24:03 +0000

    Add 30 questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,154 @@
+/*jshint esversion: 6 */
+
+const questions = [
+    {
+      question: "What is the name of the house ghost of Ravenclaw?",
+      choices: ["The Fat Friar", "The Grey Lady", "The Bloody Baron", "Nearly Headless Nick"],
+      answer: 1
+    },
+    {
+      question: "Who is the author of the book 'Fantastic Beasts and Where to Find Them'?",
+      choices: ["Albus Dumbledore", "Newt Scamander", "Rita Skeeter", "Gilderoy Lockhart"],
+      answer: 1
+    },
+    {
+      question: "What Quidditch team did Oliver Wood play for after graduating?",
+      choices: ["The Holyhead Harpies", "The Chudley Canons", "Puddlemere United", "The Falmouth Falcons"],
+      answer: 2
+    },
+    {
+      question: "How many children do Molly and Arthur Weasley have?",
+      choices: ["6 children", "7 children", "8 children", "9 children"],
+      answer: 1
+    },
+    {
+      question: "What is the core of Harry Potter's wand?",
+      choices: ["Phoenix feather", "Unicorn hair", "Dragon heartstring", "Veela hair"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the street where the Dursleys live?",
+      choices: ["Privet Drive", "Hollow Lane", "Pine Street", "Maple Avenue"],
+      answer: 0
+    },
+    {
+      question: "Which of these is not one of the Unforgivable Curses?",
+      choices: ["Avada Kedavra", "Cruciatus Curse", "Imperius Curse", "Sectumsempra"],
+      answer: 3
+    },
+    {
+      question: "What form does Hermione Granger's Patronus take?",
+      choices: ["Otter", "Stag", "Doe", "Eagle"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the potion that enables a person to transform into someone else?",
+      choices: ["Polyjuice Potion", "Veritaserum", "Felix Felicis", "Amortentia"],
+      answer: 0
+    },
+    {
+      question: "Which of these is not one of the Deathly Hallows?",
+      choices: ["Invisibility Cloak", "Elder Wand", "Resurrection Stone", "Sorcerer's Stone"],
+      answer: 3
+    },
+    {
+      question: "Who is the Half-Blood Prince?",
+      choices: ["Remus Lupin", "Severus Snape", "Sirius Black", "James Potter"],
+      answer: 1
+    },
+    {
+      question: "What is the name of the dragon that guarded the vault in Gringotts Bank?",
+      choices: ["Norbert", "Smaug", "Fang", "Ukranian Ironbelly"],
+      answer: 3
+    },
+    {
+      question: "What is the name of Draco Malfoy's mother?",
+      choices: ["Andromeda Black", "Narcissa Malfoy", "Bellatrix Lestrange", "Molly Weasley"],
+      answer: 1
+    },
+    {
+      question: "What is the name of the defense teacher in Harry's second year at Hogwarts?",
+      choices: ["Remus Lupin", "Alastor Moody", "Gilderoy Lockhart", "Severus Snape"],
+      answer: 2
+    },
+    {
+      question: "What is the maximum number of players on a Quidditch team?",
+      choices: ["5", "6", "7", "8"],
+      answer: 2
+    },
+    {
+      question: "Which creature has the ability to drain the happiness of others?",
+      choices: ["Boggart", "Dementor", "Acromantula", "Basilisk"],
+      answer: 1
+    },
+    {
+      question: "What is the name of Harry's pet owl?",
+      choices: ["Hedwig", "Crookshanks", "Scabbers", "Pigwidgeon"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the Triwizard Tournament held in Harry's fourth year?",
+      choices: ["Quidditch World Cup", "Yule Ball", "Battle of the Department of Mysteries", "Triwizard Cup"],
+      answer: 3
+    },
+    {
+      question: "Which of these is not one of the four Hogwarts Houses?",
+      choices: ["Gryffindor", "Hufflepuff", "Durmstrang", "Slytherin"],
+      answer: 2
+    },
+    {
+      question: "Who is the headmaster of Hogwarts School of Witchcraft and Wizardry throughout the series?",
+      choices: ["Albus Dumbledore", "Severus Snape", "Minerva McGonagall", "Rubeus Hagrid"],
+      answer: 0
+    },
+    {
+      question: "What is the name of Hagrid's pet dragon?",
+      choices: ["Norbert", "Smaug", "Fang", "Ukranian Ironbelly"],
+      answer: 0
+    },
+    {
+      question: " What is the password to get from Hogwarts to Hogsmeade?",
+      choices: ["Alohomora", "Fortuna Major", "Dissendium", "Lemon Drop"],
+      answer: 2
+    },
+    {
+      question: "What is the name of the wizarding prison guarded by Dementors?",
+      choices: ["Azkaban", "Nurmengard", "Arkham Asylum", "Shawshank"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the wizarding village near Hogwarts School?",
+      choices: ["Hogsmeade", "Godric's Hollow", "Diagon Alley", "Spinner's End"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the house-elf who served the Black family?",
+      choices: ["Dobby", "Winky", "Kreacher", "Hokey"],
+      answer: 2
+    },
+    {
+      question: "Who is the potions teacher in Harry's sixth year at Hogwarts?",
+      choices: ["Horace Slughorn", "Severus Snape", "Pomona Sprout", "Sybill Trelawney"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the village where the Riddle House is located?",
+      choices: ["Little Hangleton", "Budleigh Babberton", "Godric's Hollow", "Tinworth"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the device used for time travel in Harry's third year?",
+      choices: ["Time-Turner", "Time-Twister", "Time-Stopper", "Time-Shifter"],
+      answer: 0
+    },
+    {
+      question: "Which of these is not a magical creature?",
+      choices: ["Basilisk", "Kappa", "Hippogriff", "Grindylow"],
+      answer: 3
+    },
+    {
+      question: "What is the name of the spell used to summon objects to the caster?",
+      choices: ["Accio", "Expelliarmus", "Stupefy", "Lumos"],
+      answer: 0
+    }
+  ];
