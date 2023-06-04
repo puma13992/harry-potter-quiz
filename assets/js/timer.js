bd87c7f0156040730bb3d5fb4743a44ce3163e22
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-06-04 09:05:18 +0000

    Add function to countdown

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -27,3 +27,34 @@ function startTimer() {
         }
     }, 1000);
   }
+
+  /**
+ * Function to countdown the timeLeft and check if the timeLeft is 0. If no time left then the timer stops and
+ * all answer buttons are disabled so the player can't select an answer. It also loops through the answerButtons
+ * and displays the correct answer to the player if the time is up.
+ */
+function countdown() {
+    let correctAnswer = shuffledQuestions[questionIndex].answer;
+    let answerButtons = document.getElementsByClassName("choices");
+    
+    if (timeLeft === 0) {
+      stopTimer();
+      nextQuestionButton.style.display = "initial";
+      answer1.style.pointerEvents = "none";
+      answer2.style.pointerEvents = "none";
+      answer3.style.pointerEvents = "none";
+      answer4.style.pointerEvents = "none";
+      incrementWrongScore();  
+      } else {
+        timeLeft -= 1;
+      }
+    
+    for (let i = 0; i < answerButtons.length; i++) {
+      if (answerButtons[i].value === correctAnswer && timeLeft === 0) {
+        document.getElementById("answer" + (correctAnswer + 1) + "-btn").style.backgroundColor = "green";
+      } else if (answerButtons[i] !== correctAnswer && timeLeft === 0) {
+        document.getElementById("answer" + (correctAnswer + 1) + "-btn").style.backgroundColor = "green";
+      }
+    }
+  }
+  
