c4bda51827383fb1836edd9b85e46f1c0e8d64e8
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-06-04 08:38:51 +0000

    Add event listener to run the start section

---
+++ assets/js/timer.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+
+/** Add an event listener to the document 
+ * and run the start section with username log-in
+ */ 
+document.addEventListener('DOMContentLoaded', function () {
+    runStartSection();
+  });
