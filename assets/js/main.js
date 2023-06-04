0db9e5266065a43a1a8f481764e00bff7aa1f272
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-06-04 08:45:20 +0000

    Set up level button variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,9 @@ const errorMessage = document.getElementById("error-message");
 const difficultySection = document.getElementById("difficulty-section");
 const quizSection = document.getElementById("quiz-section");
 const endSection = document.getElementById("end-section");
+
+// Set up level button variables
+let easyBtn = document.getElementById("easy-btn");
+let mediumBtn = document.getElementById("medium-btn");
+let hardBtn = document.getElementById("hard-btn");
+let proBtn = document.getElementById("pro-btn");
