b1ff1f3cf2823e0c4449c113cb5be09aa7448f33
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-06-04 09:04:20 +0000

    Set up timer variables

---
+++ assets/js/timer.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+/**
+ * Timer code used from the Astronomy quiz project
+ * https://github.com/MikeR94/CI-Project-Portfolio-2/blob/main/assets/js/timer.js 
+ * and adapted with small parts of own code
+ */
+
+// Variables
+const gameTimer = document.getElementById("timer");
+let timeLeft;
+let timer;
